08e9653eace09c6d7bdf5e427b70c2d36fb6e34b
celest6667 <celestin.petit@my-digital-school.org>
2025-09-11 20:34:24 +0200

    f

---
+++ public/main.js
@@ -0,0 +1,32 @@
+import '../styles/style.css';
+import { initSphere } from './three/sphere.js';
+import { initSphere2 } from './three/sphere2.js';
+
+
+const heureEl = document.querySelector('.heure-actuelle');
+
+function mettreAJourHeure() {
+  const maintenant = new Date();
+  const heures = String(maintenant.getHours()).padStart(2, '0');
+  const minutes = String(maintenant.getMinutes()).padStart(2, '0');
+  const secondes = String(maintenant.getSeconds()).padStart(2, '0');
+  heureEl.textContent = `${heures}:${minutes}:${secondes}`;
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const isMobile = /Android|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+
+  if (isMobile) {
+    // Redirige vers le site mobile complet
+    window.location.href = "https://portfolio-v0-mobile.vercel.app";
+  }
+});
+
+
+
+mettreAJourHeure();
+setInterval(mettreAJourHeure, 1000);
+
+initSphere();
+initSphere2();
+
